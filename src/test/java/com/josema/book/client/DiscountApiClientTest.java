c92f98cb0ab88893faa4e35cf78d557bc0b432a8
Jose Manuel <j.ochoa317@gmail.com>
2024-07-21 09:13:00 -0500

    test: fix unit tests for DiscountApiClient

--- src/test/java/com/josema/book/client/DiscountApiClientTest.java
+++ src/test/java/com/josema/book/client/DiscountApiClientTest.java
@@ -1,26 +1,24 @@
 package com.josema.book.client;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
-import org.springframework.test.context.ActiveProfiles;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.josema.book.dto.DiscountRequest;
+import com.josema.book.dto.DiscountResponse;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class DiscountApiClientTest {
   @Mock
   private RestTemplate restTemplate;
@@ -29,23 +27,29 @@ public class DiscountApiClientTest {
   @InjectMocks
   private DiscountApiClient discountApiClient;
 
-  @BeforeEach
-  void setUp() {
-    MockitoAnnotations.openMocks(this);
-  }
-
   @Test
   void validateDiscountThrowsRestClientException() {
-    DiscountRequest discountRequest = new DiscountRequest("123", "456", "D0542A23");
+    DiscountRequest discountRequest = new DiscountRequest("14564088-4", "213132", "D0542A23");
     String discountUrl = "http://mock-url";
     when(environment.getProperty("DISCOUNT_URL")).thenReturn(discountUrl);
     when(restTemplate.postForObject(
-        anyString(),
-        any(DiscountRequest.class),
-        any()))
+        discountUrl, discountRequest, DiscountResponse.class))
         .thenThrow(RestClientException.class);
 
     assertThrows(RestClientException.class, () -> discountApiClient.validateDiscount(discountRequest));
     verify(environment).getProperty("DISCOUNT_URL");
+  }
+
+  @Test
+  void validateDiscountAndReturnDiscountResponse(){
+    DiscountRequest discountRequest = new DiscountRequest("14564088-4", "213132", "D0542A23");
+    DiscountResponse discountResponse = new DiscountResponse("2342", "D0542A23", 516, "2345678-3", true);
+    String discountUrl = environment.getProperty("DISCOUNT_URL");
+    when(restTemplate.postForObject(discountUrl, discountRequest, DiscountResponse.class))
+      .thenReturn(discountResponse);
+    DiscountResponse response = discountApiClient.validateDiscount(discountRequest);
+    
+    assertNotNull(response);
+    assertEquals(discountResponse, response);
   }
 }
